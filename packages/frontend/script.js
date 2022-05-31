c46bef73bb41234a4aa955a738f4b72c0224ce84
lukasPokryvka24 <87475342+lukasPokryvka24@users.noreply.github.com>
2022-05-31 10:08:29 +0200

    added render to fe funcionality

--- packages/frontend/script.js
+++ packages/frontend/script.js
@@ -4,38 +4,46 @@ const listItem = document.querySelector(".todotask");
 const todoList = document.querySelector("#todolist");
 const isChecked = document.querySelector(".cbox4:checked");
 
-let todos = [];
+let todos = null;
 
-function createTodo(todo) {
-  const newTodoItem = document.createElement("li");
-  newTodoItem.setAttribute("class", "container");
-  newTodoItem.innerHTML = `
-    <aside class="round">
-            <input
-              type="checkbox"
-              onchange="\${toggleCheck()}"
-              class="cbox4"
-            />
-            <label for="checkbox"></label>
-          </aside>
-          <section class="todotask">
-            <label class="todotask-text" for="cbox4">${todo.title}</label>
-            <div class="buttons">
-              <span class="edit">
-                <i class="fa fa-pencil" aria-hidden="true"></i>
-              </span>
-              <span class="delete">
-                <i class="fa fa-times" aria-hidden="true"></i>
-              </span>
-            </div>
-          </section>
-    `;
-  return newTodoItem;
+function createTask(task) {
+  const li = document.createElement("li");
+
+  const input = document.createElement("input");
+  input.classList.add("checkbox-round");
+  input.type = "checkbox";
+  task.isDone === 1 ? (input.checked = true) : (input.checked = false);
+
+  const section = document.createElement("section");
+
+  const h3 = document.createElement("h3");
+  h3.innerText = task.title;
+
+  const div = document.createElement("div");
+
+  const spanPencil = document.createElement("span");
+  const spanCross = document.createElement("span");
+
+  div.append(spanPencil);
+  div.append(spanCross);
+  section.append(h3);
+  section.append(div);
+  li.append(input);
+  li.append(section);
+
+  return li;
 }
-function renderTodos() {
-  todos.forEach((todo) => {
-    const newTodo = createTodo(todo);
-    todoList.appendChild(newTodo);
+
+async function renderTasks() {
+  const tasks = await fetchTasks();
+
+  while (todoList.firstChild) {
+    todoList.firstChild.remove();
+  }
+
+  tasks.forEach((task) => {
+    const taskElement = createTask(task);
+    todoList.appendChild(taskElement);
   });
 }
 
@@ -48,11 +56,10 @@ function toggleCheck(event) {
   }
 }
 
-function fetchTasks() {
-  fetch("http://localhost:3001/tasks")
-    .then((response) => response.json())
-    .then((data) => (todos = data))
-    .then(() => console.log(todos));
+async function fetchTasks() {
+  const response = await fetch("http://localhost:3001/tasks");
+  const data = await response.json();
+  return data;
 }
-fetchTasks();
-// renderTodos();
+
+renderTasks();
